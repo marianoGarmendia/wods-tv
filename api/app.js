c091a2b903cb96b30b7f10d403f3114052c13669
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-04-27 02:21:56 -0300

    key added

--- api/app.js
+++ api/app.js
@@ -13,11 +13,30 @@ const app = express();
 import { initializeApp } from "firebase/app";
 
 admin.initializeApp({
-  credential: admin.credential.cert(process.env.CREDENTIALS),
+  credential: admin.credential.cert({
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_provider_x509_cert_url: process.env.CLIENT_PROVIDER_X509_CERT_URL,
+    universe_domain: process.env.UNIVERSE_DOMAIN,
+  }),
   databaseURL: process.env.DATABASE_URL,
 });
 
-initializeApp(process.env.FIREBASE_CONFIG);
+initializeApp({
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+});
 
 // const storage = getStorage();
 // const storageRef = ref(
