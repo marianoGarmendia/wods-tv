149459a37a47e08a2ebf89e42d2edf3853aecdaf
Mariano Garmendia <marianogarmendia77@gmail.com>
2025-02-23 20:24:10 -0300

    message love

--- client/src/pages/initPage.jsx
+++ client/src/pages/initPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Workout from "../components/Workout";
-import imgLogo from "../img/logo-g10.png";
+// import imgLogo from "../img/logo-g10.png";
 
 function InitPage() {
   const [wods, setWods] = useState([]);
@@ -42,7 +42,8 @@ function InitPage() {
   return (
     <section className="h-screen w-full">
       <header className="w-full h-[10%]  flex items-center justify-center p-2">
-        <img src={imgLogo} alt="" className="h-[100%]" />
+        {/* <img src={imgLogo} alt="" className="h-[100%]" /> */}
+        <h1>♾️🐞🌊🏖️🛫⛰️♥️♾️ HEFDTLM SIEMPRE ♾️🐞🌊🏖️🛫⛰️♥️♾️</h1>
       </header>
       <div
         className="flex tracking-widest w-full justify-center min-h-[10%] flex-wrap"
